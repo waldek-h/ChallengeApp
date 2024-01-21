342d2b11955df89cd64511c9d01d0fb1177e2dcd
waldek-h <w.habinski@gmail.com>
2024-01-22 00:36:44 +0100

    Zadanie dzień 6

---
+++ ChallengeApp/ChallengeApp/Employee.cs
@@ -0,0 +1,28 @@
+﻿//1. Stwórz klasę Employee, w której przechowaz imię, nazwisko, wiek
+//    oraz punkty pracownika w postaci liczb całkowitych.
+
+namespace ChallengeApp
+{
+    internal class Employee
+    {
+        public Employee(string firstName, string lastName, int age)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Age = age;
+        }
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+        public int Age { get; set; }
+
+        private List<int> score = new List<int>();
+        public void AddScore(int score)
+        {
+            this.score.Add(score);
+        }
+        public int Result
+        {
+            get { return score.Sum(); }
+        }
+    }
+}
--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,86 +1,47 @@
-﻿int number = 500584459;
-string numberInString = number.ToString();
-char[] letters = numberInString.ToArray();
+﻿//2. Stwórz 3 pracowników i każdemu przydziel po 5 ocen z zakresu od 1 do 10.
+//3. Napisz program, który wyszuka pracownka z najwyższą liczbą ocen a nastpnie wyświetli jego dane oraz wyniki.
 
-for (int i = 0; i < letters.Length; i++)
+using ChallengeApp;
+Employee employee1 = new Employee("Kamil", "Nowak", 28);
+Employee employee2 = new Employee("Michał", "Kowalski", 35);
+Employee employee3 = new Employee("Bartek", "Rozwadowski", 31);
+
+employee1.AddScore(5);
+employee2.AddScore(4);
+employee3.AddScore(5);
+employee1.AddScore(6);
+employee2.AddScore(8);
+employee3.AddScore(9);
+employee1.AddScore(2);
+employee2.AddScore(3);
+employee3.AddScore(7);
+employee1.AddScore(5);
+employee2.AddScore(1);
+employee3.AddScore(2);
+employee1.AddScore(8);
+employee2.AddScore(1);
+employee3.AddScore(6);
+
+List<Employee> employeeList = new List<Employee>() { employee1, employee2, employee3 };
+
+int maxResult = -1;
+Employee employeeWithMaxResult = null;
+
+foreach (var empleyee in employeeList)
 {
-    var counter = 0;
-    foreach (char letter in letters)
+    if (empleyee.Result > maxResult)
     {
-        if (i == letter - '0')
-        {
-            counter++;
-        }
+        employeeWithMaxResult = empleyee;
     }
-    Console.WriteLine(i + " => " + counter);
 }
 
+Console.WriteLine($"Najwyższą liczbę ocen w wysokości {employeeWithMaxResult.Result} osiągnął pracownik {employeeWithMaxResult.FirstName} {employeeWithMaxResult.LastName}.\n");
 
+Console.WriteLine($"Oceny poszczeólnych pracowników:\n");
 
+foreach (var empleyee in employeeList)
+{
+    Console.WriteLine($"{empleyee.FirstName} {empleyee.LastName} - {empleyee.Result}");
 
-//int counter0 = 0;
-//int counter1 = 0;
-//int counter2 = 0;
-//int counter3 = 0;
-//int counter4 = 0;
-//int counter5 = 0;
-//int counter6 = 0;
-//int counter7 = 0;
-//int counter8 = 0;
-//int counter9 = 0;
+}
 
-//foreach (char letter in letters)
-//{
-//    if (letter == '0')
-//    {
-//        counter0++;
-//    }
-//    else if (letter == '1')
-//    {
-//        counter1++;
-//    }
-//    else if (letter == '2')
-//    {
-//        counter2++;
-//    }
-//    else if (letter == '3')
-//    {
-//        counter3++;
-//    }
-//    else if (letter == '4')
-//    {
-//        counter4++;
-//    }
-//    else if (letter == '5')
-//    {
-//        counter5++;
-//    }
-//    else if (letter == '6')
-//    {
-//        counter6++;
-//    }
-//    else if (letter == '7')
-//    {
-//        counter7++;
-//    }
-//    else if (letter == '8')
-//    {
-//        counter8++;
-//    }
-//    else if (letter == '9')
-//    {
-//        counter9++;
-//    }
-//}
-
-
-//Console.WriteLine(0 + " => " + counter0);
-//Console.WriteLine(1 + " => " + counter1);
-//Console.WriteLine(2 + " => " + counter2);
-//Console.WriteLine(3 + " => " + counter3);
-//Console.WriteLine(4 + " => " + counter4);
-//Console.WriteLine(5 + " => " + counter5);
-//Console.WriteLine(6 + " => " + counter6);
-//Console.WriteLine(7 + " => " + counter7);
-//Console.WriteLine(8 + " => " + counter8);
-//Console.WriteLine(9 + " => " + counter9);
